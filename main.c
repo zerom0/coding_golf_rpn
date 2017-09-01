cbc35e9711453830b46dc27eba8e51d751b4c1e8
zerom0 <github@martinmosler.de>
2017-09-01 11:18:28 +0200

    139

--- main.c
+++ main.c
@@ -1,15 +1,14 @@
-#define X(x) if(*v[i]==*#x) {s[p-1]x##=s[p]; continue;}
+#define X(x) if(*v[i]==*#x) s[p-1]x##=s[--p]; else
+
 p, i, s[];
+
 main(c,  v) char** v; {
-  while (++i<c) {
-    --p;
+  while (++i < c)
     X(+)
     X(-)
     X(*)
     X(/)
-    s[++p] = atoi(v[i]);
-    p++;
-}
+    s[p++] = atoi(v[i]);
 
   printf("%d", s[p-1]);
 }
